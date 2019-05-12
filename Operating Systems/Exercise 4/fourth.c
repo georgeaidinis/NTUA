d1d365829987d1371657884609500a277bebdc34
George Aidinis <gaidinis@live.com>
2019-05-12 13:24:50 +0300

    some more cleaning up on ex 4

--- Operating Systems/Exercise 4/fourth.c
+++ Operating Systems/Exercise 4/fourth.c
@@ -7,49 +7,49 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void critical_section(int i,pid_t child_ID,int my pipe[],int pipe1[],int pipe2[]){
+void critical_section(int i,pid_t child_ID,int my_pipe[]){
 	int j,x;
 	int cont = 1;
 	int stop = 0;
 	while(true){
-		read(my pipe[0],&x, sizeof(stop));
+		read(my_pipe[0],&x, sizeof(stop));
 		if(x>0){
-			write(my pipe[1],&stop,sizeof(stop));
+			write(my_pipe[1],&stop,sizeof(stop));
 			printf("\n");
 			for (j=1; j<6; j++){
 				printf("Child %d, pID: %d, executes a Critical section, for the %d time.\n",i+1, child_ID, j);
 			}
 			printf("\n");
-			write(my pipe[1],&cont,sizeof(stop));
+			write(my_pipe[1],&cont,sizeof(stop));
 			return;
 		}
-		write(my pipe[1],&x,sizeof(stop));
+		write(my_pipe[1],&x,sizeof(stop));
 	}
 }
-void non_critical_section(int i,pid_t child_ID,int my pipe[],int pipe1[],int pipe2[],int times){
+void non_critical_section(int i,pid_t child_ID,int my_pipe[],int times){
 	int x;
 	int cont = 1;
 	if (times==0)
 		return;
-	read(my pipe[0],&x, sizeof(cont));
+	read(my_pipe[0],&x, sizeof(cont));
 	if(x>0){
 		printf("Child %d, pID: %d, executes a Non - Critical section, for the %d time.\n",i+1, child_ID, 8-times);
-		write(my pipe[1],&x,sizeof(cont));
-		non_critical_section(i,child_ID,my pipe,times-1);
+		write(my_pipe[1],&x,sizeof(cont));
+		non_critical_section(i,child_ID,my_pipe,times-1);
 	}
 	else{
-		write(my pipe[1],&x,sizeof(cont));
-		non_critical_section(i,child_ID,my pipe,times);
+		write(my_pipe[1],&x,sizeof(cont));
+		non_critical_section(i,child_ID,my_pipe,times);
 	}
 }
 int main (int argc, char ** argv){
 	int i,x, status;
 	int cont = 1;
 	pid_t Child_ID[3];
-	int my pipe[2];
-	if (pipe(my pipe) == -1)
+	int my_pipe[2];
+	if (pipe(my_pipe) == -1)
    		printf("Error on pipe creation\n"); //0 on success, -1 on error.
-   	write(my pipe[1],&cont,sizeof(cont));
+   	write(my_pipe[1],&cont,sizeof(cont));
 	for(i=0;i<3;i++){
 		x = fork();
 		if (x>0){
@@ -57,34 +57,34 @@ int main (int argc, char ** argv){
 		}
 		else if (x==0 && i==0){
 			pid_t child1 = getpid();
-	    	critical_section(0,child1,my pipe);
-	    	non_critical_section(0,child1,my pipe,7);
-	    	non_critical_section(0,child1,my pipe,7);
-	    	critical_section(0,child1,my pipe);
-	    	non_critical_section(0,child1,my pipe,7);
-	    	critical_section(0,child1,my pipe);
+	    	critical_section(0,child1,my_pipe);
+	    	non_critical_section(0,child1,my_pipe,7);
+	    	non_critical_section(0,child1,my_pipe,7);
+	    	critical_section(0,child1,my_pipe);
+	    	non_critical_section(0,child1,my_pipe,7);
+	    	critical_section(0,child1,my_pipe);
     		exit(0);
 			break;
 		}
 		else if (x==0 && i==1){
 			pid_t child2 =  getpid();
-			non_critical_section(1,child2,my pipe,7);
-	     	critical_section(1,child2,my pipe);
-	     	non_critical_section(1,child2,my pipe,7);
-	     	critical_section(1,child2,my pipe);
-	     	non_critical_section(1,child2,my pipe,7);
-	     	critical_section(1,child2,my pipe);
+			non_critical_section(1,child2,my_pipe,7);
+	     	critical_section(1,child2,my_pipe);
+	     	non_critical_section(1,child2,my_pipe,7);
+	     	critical_section(1,child2,my_pipe);
+	     	non_critical_section(1,child2,my_pipe,7);
+	     	critical_section(1,child2,my_pipe);
 			exit(0);
 			break;
 		}
 		else if (x==0 && i==2){
 			pid_t child3 = getpid();
-	     	critical_section(2,child3,my pipe);
-	     	non_critical_section(2,child3,my pipe,7);
-	     	critical_section(2,child3,my pipe);
-	     	non_critical_section(2,child3,my pipe,7);
-	     	critical_section(2,child3,my pipe);
-	     	non_critical_section(2,child3,my pipe,7);
+	     	critical_section(2,child3,my_pipe);
+	     	non_critical_section(2,child3,my_pipe,7);
+	     	critical_section(2,child3,my_pipe);
+	     	non_critical_section(2,child3,my_pipe,7);
+	     	critical_section(2,child3,my_pipe);
+	     	non_critical_section(2,child3,my_pipe,7);
 			exit(0);
 			break;
 		}
