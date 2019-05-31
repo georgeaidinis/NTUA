616adf1ea71debee722142b22628cda3a3c5f493
George Aidinis <gaidinis@live.com>
2019-05-31 15:40:54 +0300

    finished insert, edited update.

--- Databases/mysql_insert.py
+++ Databases/mysql_insert.py
@@ -11,7 +11,7 @@ The new row contains the attributes ISBN(Varchar 45, KEY),
 Title (Varchar 45), Pages(Int), Publication Year (Date),
 Publishers_Name(Varchar45, Not Null) and Library Name(Varchar 45).
 """
-def insert_book(ISBN, Title, Pages, Publication_Year, Publishers_Name, Library_Name):
+def insert_book(ISBN, Title, Pages, Publication_Year, Publishers_Name, Library_Name, Author):
 	query = "INSERT INTO Books VALUES (%s,%s,%s,%s,%s,%s)"
 	args = (ISBN, Title, Pages, Publication_Year, Publishers_Name, Library_Name)
 	try:
@@ -131,21 +131,95 @@ def insert_Member(MemberID,Name,Surname,Address,Number_of_Books,Birthdate,Can_Bo
 		cursor.close()
 		conn.close()
 
+"""
+Inserts a row into the Authors relation of the DB by creating
+a query in sql format.Creates Connection, finds configuration.
+The new row contains the attributes AuthorID (INT, automated KEY),
+Name(Varchar 45), Surname(Varchar 45), Birthdate(DATE)
+"""
+
+def insert_Author(Name, Surname, Birthdate):
+	query = "INSERT INTO Authors(Name, Surname, Birthdate) VALUES(  %s , %s , %s )"
+	args = (Name, Surname, Birthdate)
+	try:
+		db_config = read_db_config()
+		conn = MySQLConnection(**db_config)
+
+		cursor = conn.cursor()
+		cursor.execute(query, args)
+
+		if cursor.lastrowid:
+			print('done')
+		else:
+			print('last insert id not found')
+
+		conn.commit()
+	except Error as error:
+		print(error)
+
+	finally:
+		cursor.close()
+		conn.close()
+
+
+
+
+"""
+Inserts a row into the Staff relation of the DB by creating
+a query in sql format.Creates Connection, finds configuration.
+The new row contains the attributes StaffID (INT, automated KEY),
+Name(Varchar 45),Pay (INT), Surname(Varchar 45) and 
+LibraryName(Varchar 45).
+"""
+def insert_Staff(Name,Pay,Surname ,LibraryName):
+	query = "INSERT INTO Staff(Name,Pay,Surname ,LibraryName) VALUES(  %s , %s , %s , %s)"
+	args = ( Name, Pay, Surname, LibraryName)
+	try:
+		db_config = read_db_config()
+		conn = MySQLConnection(**db_config)
+
+		cursor = conn.cursor()
+		cursor.execute(query, args)
+
+		if cursor.lastrowid:
+			print('done')
+		else:
+			print('last insert id not found')
+
+		conn.commit()
+	except Error as error:
+		print(error)
+
+	finally:
+		cursor.close()
+		conn.close()
 
 
  
 def main():
-	insert_library('NTUA')
+	"""insert_library('NTUA')
 	file_name = "publishers.csv"
 	for x in read_the_file(file_name):
 		insert_Publisher(x[0],x[1],x[2])
 	file_name = "books.csv"
 	for x in read_the_file(file_name):
-		insert_book(x[0],x[1],x[2],x[3],x[4],x[5])
+		insert_book(x[0],x[1],x[2],x[3],x[4],x[5],x[6])
 	file_name = "users.csv"
 	for x in read_the_file(file_name):
 		insert_Member(x[0],x[1],x[2],x[3],x[4],x[5],x[6])
+	insert_Author(1,"Edgar Allan", "Poe", "1809-01-19")
+	insert_Staff("Theodor", 42,"Tsilivis", "NTUA")
+	insert_Staff("George", 4200,"Aidinis", "NTUA")
+	insert_Staff("Pheivws", 420,"Kalogiannis", "NTUA")
+	insert_Staff("Donald John", 3,"Trump", "NTUA")
+	insert_Staff("Melania", 4,"Trump", "NTUA")
+	insert_Staff("Ivanka", 5,"Trump", "NTUA")
+	insert_Staff("Donald Jr", 6,"Trump", "NTUA")
+	insert_Staff("Tiffany", 7,"Trump", "NTUA")
+	insert_Staff("Eric", 8,"Trump", "NTUA")"""
 	
+
+
  
 if __name__ == '__main__':
     main()
--- Databases/mysql_update.py
+++ Databases/mysql_update.py
@@ -4,9 +4,94 @@ from mysql.connector import MySQLConnection, Error
 from mysql_dbconfig import read_db_config
 
 
-def update_books (ISBN, Title, Pages, Publication_Year, Publishers_Name, Library_Name, ISBN_KEY):
-	query = "UPDATE Books SET Title ='Ta arxidia moy KOUNIOUNTAI' WHERE Pages=1301;"
-	#args = (Title)
+def update_books (ISBN = '', Title = '', Pages = 0, Publication_Year = 500, Publisher = '', Library = '', ISBN_KEY = '',  condition = ''):
+	coma_counter = [0]*6
+	attribute_list = ["ISBN = '", "Title = '", "Pages = ", "Publication_Year = ", "Publishers_Name = '", "LibraryName = '"]
+	i = 0
+	
+	if ISBN != '':
+		coma_counter[0] +=1
+	if Title != '':
+		coma_counter[1] +=1
+	if Pages != 0:
+		coma_counter[2] +=1
+	if Publication_Year != 500:
+		coma_counter[3] +=1
+	if Publisher != '':
+		coma_counter[4] +=1
+	if Library != '':
+		coma_counter[5] +=1
+	
+	j = sum(coma_counter)
+	List = ["UPDATE Books SET "]
+
+	if ISBN != '' and j!=1:
+		List += attribute_list[0]
+		List += ISBN
+		List += "',"
+		j = j-1
+	elif ISBN != '' and j==1: 
+		List += attribute_list[0]
+		List += ISBN
+		List += "'"
+
+	
+	if Title != '' and j!=1:
+		List += attribute_list[1]
+		List += Title
+		List += "',"
+		j = j-1
+	elif Title != '' and j==1: 
+		List += attribute_list[1]
+		List += Title
+		List += "'"
+
+
+	if Pages != 0 and j!=1:
+		List += attribute_list[2]
+		List += Pages
+		List += "',"
+		j = j-1
+	elif Pages != 0 and j==1: 
+		List += attribute_list[2]
+		List += Pages
+		List += "'"
+
+
+	if Publication_Year != 500 and j!=1:
+		List += attribute_list[3]
+		List += Publication_Year
+		List += "',"
+		j = j-1
+	elif Publication_Year != 500 and j==1: 
+		List += attribute_list[3]
+		List += Publication_Year
+		List += "'"
+
+	if Publisher != '' and j!=1:
+		List += attribute_list[4]
+		List += Publisher
+		List += "',"
+		j = j-1
+	elif Publisher != '' and j==1: 
+		List += attribute_list[4]
+		List += Publisher
+		List += "'"
+
+	elif Library != '' and j==1: 
+		List += attribute_list[5]
+		List += Library
+		List += "'"
+
+
+	List += "Where "
+	if ISBN_KEY != '':
+		List += ISBN_KEY
+	else:
+		List += condition
+	List += ";"
+
+	query = ''.join(List)
 	try:	
 		db_config = read_db_config()
 		conn = MySQLConnection(**db_config)
@@ -26,8 +111,10 @@ def update_books (ISBN, Title, Pages, Publication_Year, Publishers_Name, Library
 	finally:
 		cursor.close()
 		conn.close()
+
+
+
 def main():
-	update_books("960-02-1088-8","Ta arxidia moy KOUNIOUNTAI",1301,1994,"Εκδοτικός Οίκος Φυσικής","NTUA","960-02-1088-8")
- 
+	
 if __name__ == '__main__':
     main()
