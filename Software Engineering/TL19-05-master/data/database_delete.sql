37920594bc32a8285000e2aacec821bd53852a1f
georgeaidinis <gaidinis@live.com>
2020-09-03 15:28:26 +0300

    oopsie

--- Software Engineering/TL19-05-master/data/database_delete.sql
+++
@@ -1,9 +0,0 @@
-USE ElectroMarket;
-
-DROP DATABASE IF EXISTS ElectroMarket;
-
--- DROP TABLE IF EXISTS AllocatedEICDetail;
--- DROP TABLE IF EXISTS AreaTypeCode;
--- DROP TABLE IF EXISTS MapCode;
--- DROP TABLE IF EXISTS ProductionType;
--- DROP TABLE IF EXISTS ResolutionCode;
--- Software Engineering/TL19-05-master/get_data.sh
+++
@@ -1,3 +0,0 @@
-wget https://www.dropbox.com/s/zn7ajtlgdkp0vi3/referenceTables.zip
-wget https://www.dropbox.com/s/kn8mwyize7jv0hb/10daysData.zip
-wget https://www.dropbox.com/s/kwmfreqnn1kmkqg/10days\(values\).xlsx
